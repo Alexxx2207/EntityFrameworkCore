3871336ac483503c0d104512e1659668860d223b
Alexxx2207 <alex.ivanov0722@gmail.com>
2021-10-28 13:29:17 +0300

    Entity Relations
    I've used data annotation attributes and Fluent API to control the initialization of the two databases.

---
+++ Entity Relations/ConnectionStringInfo/ConnectionStringInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConnectionStringInfo
+{
+    public static class ConnectionStringInfo
+    {
+        private static string connection = "";
+
+        public static string ConnectionString
+        {
+            get 
+            {
+                if (connection == "")
+                    connection = BuildConnectionString();
+
+                return connection;
+            }
+        }
+
+        private static string BuildConnectionString()
+        {
+            var reader = new StreamReader(@"..\..\..\..\ConnectionString.ini");
+            var sb = new StringBuilder();
+
+            while (!reader.EndOfStream)
+            {
+                sb.Append(reader.ReadLine());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Entity Relations/P01_StudentSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using P01_StudentSystem.Data;
+using System;
+
+namespace P01_StudentSystem
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
